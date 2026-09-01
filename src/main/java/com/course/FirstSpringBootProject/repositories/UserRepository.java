9f5e79d0ed12245a9279c4207ac0b9377f08c005
Ricardo Fernandes <ricardo.b.fernandes@accenture.com>
2026-09-01 12:22:51 -0300

    Service layer, component registration

--- src/main/java/com/course/FirstSpringBootProject/repositories/UserRepository.java
+++ src/main/java/com/course/FirstSpringBootProject/repositories/UserRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.course.FirstSpringBootProject.entities.User;
 
 public interface UserRepository extends JpaRepository<User, Long>{ //vai ser capaz de instanciar pra gente um objeto repository que vai ter várias operações para trabalhar com usuário
-	
+
 
 }
